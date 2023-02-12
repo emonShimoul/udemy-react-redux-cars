b62af0325bb30d829b860f29cf8f458a551bf876
emonShimoul <emonshimoul@gmail.com>
2023-02-12 14:34:25 +0600

    implemented highlighting existing cars

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,8 +4,13 @@ import { removeCar } from '../store';
 
 const CarList = () => {
     const dispatch = useDispatch();
-    const cars = useSelector(({ cars: { data, searchTerm } }) => {
-        return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+        const filteredCars = data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+        return {
+            cars: filteredCars,
+            name: form.name
+        }
     });
 
     const handleCarDelete = (car) => {
@@ -13,8 +18,10 @@ const CarList = () => {
     };
 
     const renderedCars = cars.map(car => {
+        // DECIDE IF THIS CAR SHOULD BE BOLD
+        const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
